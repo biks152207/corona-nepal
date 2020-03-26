2fb4b9a0b80234fda0301ba7be8e89404b44c23f
bikram <bikram@pop-os.localdomain>
2020-03-26 10:44:16 +0545

    Updates

---
+++ precache-manifest.86cca1af9de24ec9fedf540f9e93983d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f340ec8677dcb16b8a8df8ba6363405",
+    "url": "/corona-nepal/index.html"
+  },
+  {
+    "revision": "c264f80bbc14a59debfe",
+    "url": "/corona-nepal/static/css/main.e8c70e54.chunk.css"
+  },
+  {
+    "revision": "df3b9e8bc9242c2641bc",
+    "url": "/corona-nepal/static/js/2.decf9b79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-nepal/static/js/2.decf9b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c264f80bbc14a59debfe",
+    "url": "/corona-nepal/static/js/main.863159b4.chunk.js"
+  },
+  {
+    "revision": "9f1a54826aa972f36d48",
+    "url": "/corona-nepal/static/js/runtime-main.0b470d92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona-nepal/static/media/logo.5d5d9eef.svg"
+  }
+]);
